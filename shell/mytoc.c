e69a86aa4db2f0d8c67a640d4298e57bd01330b5
[Jorge Guijosa] <[jeguijosaperez@miners.utep.edu]>
2017-09-26 01:20:52 -0600

    Works for ls, not for cd.

---
+++ shell/mytoc.c
@@ -0,0 +1,88 @@
+#include <stdlib.h>
+
+/* Function that copies a string to a certain adress sepcified by the destination pointer*/
+void strCopy(char* from, char* destination, char delim)
+{
+  char* fromPointer, *toPointer;
+  fromPointer = from;
+  toPointer = destination;
+  while((*fromPointer != delim) && (*fromPointer != '\0') && (*fromPointer != '\n')) //while there is a current token
+    {
+      *toPointer = *fromPointer; //copy that token
+      toPointer++;
+      fromPointer++;
+    }
+  *toPointer = '\0'; //set the copy's last element to o terminator
+  return;
+}
+
+/* Function that counts the lenght of a token, given a delimiter*/
+int tokenlen(char* tokenBegin, char delim)
+{
+  char* strPointer;
+  int counter;
+  for(strPointer = tokenBegin; (*strPointer != delim) && (*strPointer != '\0'); strPointer++);
+  counter = strPointer - tokenBegin + 1;
+  return counter;
+}
+
+/*Funtion to count thenumber of tokens on a string in accordance to the given delimiter*/
+int tokenCount(char* str, char delim)
+{
+  int counter = 0;
+  int tokenBool = 0;
+  char* strPointer;
+  for(strPointer = str; *strPointer != '\0'; strPointer++) //go through the string
+    {
+      if(*strPointer == delim) //if you find a delimiter
+	{
+	  if(tokenBool)  // if you are on a token
+	    {
+	      tokenBool = 0; //mark that you are not on one now
+	    }
+	}
+      else //if you find a char
+	{
+	  if(!tokenBool) //if you are not on a token
+	    {
+	      tokenBool = 1; //mark you are now on one
+	      counter++; //count one more token
+	    }
+	}
+    }
+  return counter;
+}
+
+/*Function to tokenize a given string, in accordance to a given delimiter*/
+char** mytoc(char* str, char delim)
+{
+  char* strPointer, *currentTokenBegin;
+  int numTokens = tokenCount(str,delim);  //count tokens
+  numTokens++;
+  int tokenBool = 0, currentTokenLen = 0, currTok  = 0;
+  char** tokenVec = calloc(numTokens,sizeof(char*)); // allocate mem for the vector
+  tokenVec[numTokens-1] = (char *)0; //set las place of vetor to 0 terminator
+  for(strPointer = str; *strPointer != '\0'; strPointer++) // go through string
+    {
+      if(*strPointer == delim) //if you find a delim
+	{
+	  if(tokenBool) //if on a token 
+	    {
+	      tokenBool = 0; //mark you are in none now
+	    }
+	}
+      else // if thou find a character
+	{
+	  if(!tokenBool)//if you are not on a token
+	    {
+	      tokenBool = 1;// mark you are now on a token
+	      currentTokenBegin = strPointer; //point to the beggining of said token
+	      currentTokenLen = tokenlen(currentTokenBegin,delim); //get length of said token
+	      tokenVec[currTok] = (char*)malloc(currentTokenLen*sizeof(char)); // allocate mem for said token
+	      strCopy(currentTokenBegin,tokenVec[currTok],delim); //copy token
+	      currTok++; //point to a next place in token vector.
+	    }
+	}
+    }
+  return tokenVec;
+}
--- shell/shell.c
+++ shell/shell.c
@@ -1,7 +1,46 @@
-include <unistd.h>
-include <mytoc.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include "mytoc.h"
 
 #define BUFLEN 1000
+
+/*Function to concatenate two strings*/
+char* concat(char* str1, char* str2)
+{
+  char* i, *j, *k;
+  int len1 = tokenlen(str1,' ');
+  int len2 = tokenlen(str2, ' ');
+  char* string = (char*)malloc((len1+len2)*sizeof(char));
+  k = string;
+  for( i = str1; *i != '\0' ; i++, k++)
+    {
+    *k = *i;
+    }
+  for( j = str2; *j != '\0' ; j++, k++)
+    {
+     *k = *j;
+    }
+  *k = '\0';
+  return string;
+}
+
+/*finds index for PATH variable in envp*/
+int findPathIndex(char** envp){
+  char ** courser;
+  int counter = 0;
+  for(courser = envp; *courser; courser++)
+    {
+      if(((*courser)[0] == 'P')&&((*courser)[1] == 'A')&&((*courser)[2]=='T')&&((*courser)[3]=='H'))
+	{
+	  return counter;
+	}
+      else
+	{
+	  counter++;
+	}
+    }
+}
 
 /*Function to compare two strings for equality*/
 int strEq(char* str1, char* str2)
@@ -20,74 +59,60 @@ int strEq(char* str1, char* str2)
   return 1;
 }
 
-char* findPath(char* cmd, char* path)
-{
-  int i;
-  char** paths = mytoc(path,':');
-  char* iterator;
-  char* iterator2;
-  char** directories;
-  char* directory;
-  for(iterator = path; iterator ; iterator = iterator++)
-    {
-      directories = mytoc(iterator,'/'); 
-      for(iterator2 = directories; iterator2 ; iterator2++)
-	{
-	  directory = iterator2;
-	  if(strEq(directory,cmd)
-	    {
-	      return iterator;
-	    }
-	}
-    }
-      return "0";
-}
-
-
 int main(int argc, char **argv, char **envp)
 {
+  int index;
+  int amtRead;
   int pid;
   int status;
   char buf[BUFLEN];
   char s[2];
-  char err[36]
-  char **command;
-  char* path
-  char error[18]
+  char* err;
+  char** command;
+  char** path;
+  char* error;
+  char** pathVar;
+  char* pathConcat;
+  char* commandRead;
   while(1)
     {
-      error = "Command not found."
-      err = "Program terminated with exit code";
+      error = "Command not found.\n";
+      err = "Program terminated with exit code\n";
       s[0] = '$';
       s[1] = ' ';
       write(1,s,2);
       amtRead = read(0,buf, BUFLEN);
-      for(i = 0; i < amtRead; i++)
+      buf[amtRead-1] = '\0';
+      command =  mytoc(buf,' ');
+      if(strEq(command[0],"exit"))  /*exit if you read exit*/
 	{
-	 command =  mytoc(buf," ");
-	 if(strEq(command[0],"exit"))
-	   {
-	     break;
-	   }
-	 path = findPath(command[0]);
-	 if(*path == '0')
-	   {
-	     write(1,error,18);
-	     continue;
-	   }
-	 pid = fork();
-	 if(pid == 0)
-	   {
-	     status = execve(path,command,envp);
-	     if(status < 0)
-	       {
-	        write(1, err, 36);
-	       }
-	   }
-	 else
-	   {
-	     wait();
-	   }
+	  break;
+	}
+      pid = fork();
+      if(pid == 0)
+	{
+	  index = findPathIndex(envp);
+	  pathVar = mytoc(envp[index],'=');
+	  path = mytoc(pathVar[1],':');
+	  commandRead = command[0];
+	  commandRead = concat("/",commandRead);  /*setup the possible paths and the executable directory*/
+	  while((*path) != '\0')                  /*try the mashup in every possible path*/
+	    {
+	      pathConcat = concat(*path,commandRead);
+	      command[0] = pathConcat;
+	      status = execve(pathConcat,command,envp);
+	      if(status != -1) exit(0);      
+	      path++;
+	    }
+	  if(status < 0)                          /*If you reach here command wasn't found*/
+	    {
+	      write(1, error, 20);
+	    }
+	  exit(0);
+	}
+      else
+	{
+	  wait(NULL);
 	}
     }
 }
